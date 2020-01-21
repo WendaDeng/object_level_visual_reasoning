f5f0e5a1a8ea9747220a40b8daa8895308f5fe50
WendaDeng <dwd1044898101@gmail.com>
2020-01-21 16:36:05 +0800

    change video dir

--- preprocessing/resize.py
+++ preprocessing/resize.py
@@ -5,18 +5,18 @@ import cv2
 
 
 def main():
-	for video_dir in os.listdir('videos'):
-		img_list = glob.glob(os.path.join('videos', video_dir, '*/*/*.jpg'))
+	video_dir = '/mnt/shared_40t/dwd/epic-kitchens/dataset/EPIC_KITCHENS_2018/interim/rgb_train_segments'
+	for vdir in os.listdir(video_dir):
+		img_list = glob.glob(os.path.join(video_dir, vdir, '*/*/*.jpg'))
 
 		for i in img_list:
 			img = cv2.imread(i)
 			new_img = cv2.resize(img, (256, 256))
-			img_name = i.replace('videos', 'videos_256x256_30')
+			img_name = i.replace('dataset/EPIC_KITCHENS_2018/interim/rgb_train_segments', 'orn/data/epic/videos_256x256_30')
 			img_dir = os.path.dirname(img_name)
 			if not os.path.exists(img_dir):
-				os.makedirs(img_dir, mode=0o666)
+				os.makedirs(img_dir)
 			cv2.imwrite(img_name, new_img)
-
 
 
 if __name__ == '__main__':
